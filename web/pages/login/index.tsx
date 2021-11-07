c090f50ac74973035ae616a987406d4ce5bc16e6
Nick Minor <naminor@buffalo.edu>
2021-11-07 04:37:28 -0500

    Added axios for one request

--- web/pages/login/index.tsx
+++ web/pages/login/index.tsx
@@ -15,8 +15,21 @@
   ```
 */
 
+import React from "react"
+import axios from "axios";
+const baseURL = "localhost:8000";
+
+
+
 export default function Example() {
-    return (
+  const [post, setPost] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(`${baseURL}/users`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+  
+  return (
       <>
         {/*
           This example requires updating your template:
@@ -178,7 +191,7 @@ export default function Example() {
           <div className="hidden lg:block relative w-0 flex-1">
             <img
               className="absolute inset-0 h-full w-full object-cover"
-              src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+              src="5-people-listening-to-music.png"
               alt=""
             />
           </div>
